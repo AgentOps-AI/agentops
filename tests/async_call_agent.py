359c760f6af1b848931cf60914db837ba9a59fd7
reibs <areibman@gmail.com>
2023-10-05 14:39:05 -0700

    added agent mock

--- tests/async_call_agent.py
+++ tests/async_call_agent.py
@@ -1,22 +1,66 @@
+import agentops
 import asyncio
 import openai
-from openai import ChatCompletion
 
-import agentops
+
+# from wandb.integration.openai import autolog
+# autolog()
+
 
 ao_client = agentops.Client(api_key='',
                             tags=['mock tests'])
-openai.api_key = ''
-
-message = [{"role": "user", "content": "Hello"},
-           {"role": "assistant", "content": "Hi there!"}]
 
 
-async def chat():
-    await ChatCompletion.acreate(
+openai.api_key = '3'
+
+
+async def stream_achat():
+    message = [{"role": "user", "content": "stream achat test"}]
+
+    res = await openai.ChatCompletion.acreate(
+        model='gpt-3.5-turbo', messages=message, temperature=0.5, stream=True)
+    async for r in res:
+        print(r)
+    print('*'*100)
+
+
+def stream_chat():
+    message = [{"role": "user", "content": "stream chat test"}]
+    res = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo', messages=message, temperature=0.5, stream=True)
+    for r in res:
+        print(r)
+    print('*'*100)
+
+
+async def achat():
+    message = [{"role": "user", "content": "achat test"}]
+    res = await openai.ChatCompletion.acreate(
         model='gpt-3.5-turbo', messages=message, temperature=0.5)
+    print(res)
 
-print('running async call')
-asyncio.run(chat())
-print('running async call')
+    print('*'*100)
+
+
+def chat():
+    message = [{"role": "user", "content": "chat test"}]
+    res = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo', messages=message, temperature=0.5)
+    print(res)
+
+    print('*'*100)
+
+
+print('running async stream')
+asyncio.run(stream_achat())
+
+print('running sync stream')
+stream_chat()
+
+print('running async chat')
+asyncio.run(achat())
+
+print('running sync chat')
+chat()
+
 ao_client.end_session('Success')
