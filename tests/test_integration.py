ff2649bec7f532758cbe827df443fa7ed9e95f2d
reibs <areibman@gmail.com>
2023-11-23 04:31:29 -0800

    added handler to langsmith agent

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,10 @@
 from langchain.agents import AgentType, Tool, initialize_agent
 from langchain.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
-import os
+from agentops.integrations import CallbackHandler
+from agentops import Client
+
+ao_client = Client()
 
 tools = [
     Tool(
@@ -14,14 +17,16 @@ tools = [
 
 llm = ChatOpenAI(model_name="gpt-3.5-turbo")
 
+handler = CallbackHandler(client=ao_client)
 agent_executor = initialize_agent(
     tools=tools,
     llm=llm,
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose=True,
     handle_parsing_errors=True,
-    memory=ConversationBufferMemory(memory_key="chat_history")
+    memory=ConversationBufferMemory(memory_key="chat_history"),
+    callbacks=[handler]
 )
 
-agent_executor.invoke(
-    {"input": "what are some movies showing 9/21/2023?"})["output"]
+agent_executor.run("what are some movies showing 9/21/2023?",
+                   callbacks=[handler])
