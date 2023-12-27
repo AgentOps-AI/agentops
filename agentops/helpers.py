8c810a76cc16d72c6e762a428c367d245156ad97
reibs <areibman@gmail.com>
2023-12-26 20:56:18 -0800

    added docstring

--- agentops/helpers.py
+++ agentops/helpers.py
@@ -4,6 +4,12 @@ from datetime import datetime
 
 
 def get_ISO_time():
+    """
+    Get the current UTC time in ISO 8601 format with milliseconds precision, suffixed with 'Z' to denote UTC timezone.
+
+    Returns:
+        str: The current UTC time as a string in ISO 8601 format.
+    """
     return datetime.utcfromtimestamp(time.time()).isoformat(timespec='milliseconds') + 'Z'
 
 
