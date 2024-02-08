b671433beb43c1440ff2138b02f7706f45a947df
reibs <areibman@gmail.com>
2024-02-07 20:28:51 -0800

    update langchain callbacks

--- agentops/langchain_callback_handler.py
+++ agentops/langchain_callback_handler.py
@@ -1,21 +1,92 @@
 from typing import Dict, Any, List, Optional, Union
 from uuid import UUID
+import re
 
 from langchain_core.agents import AgentFinish, AgentAction
 from langchain_core.outputs import LLMResult
 from langchain_core.documents import Document
+from langchain_core.messages import BaseMessage, SystemMessage, AIMessage, HumanMessage
+
 
 from langchain_core.outputs import LLMResult
 
 
 from agentops import Client as AOClient
-from agentops import Event
+from agentops.event import Event, ChatML, ChatMLItem, CompletionResponse, UsageItem, ChoiceItem
 from tenacity import RetryCallState
 
 from langchain.callbacks.base import BaseCallbackHandler, AsyncCallbackHandler
 
 from agentops.helpers import get_ISO_time
 from typing import Any, Dict, List, Optional, Sequence
+
+
+def prompt_to_chatml(prompt: str) -> ChatML:
+    # Define the pattern to match the roles and the messages
+    pattern = r'(System|Human|Assistant):\s(.*?)(?=\n(?:System|Human|Assistant):|$)'
+
+    # Define a mapping from the found roles to the desired roles
+    role_mapping = {'System': 'system',
+                    'Human': 'user',
+                    'Assistant': 'assistant'}
+
+    # Find all matches in the prompt
+    matches = re.findall(pattern, prompt, re.DOTALL)
+
+    # Create a list of dictionaries with the role and content
+    prompt_list: ChatML = [{"role": role_mapping[role], "content": message.strip()}
+                           for role, message in matches]
+
+    return prompt_list
+
+
+def message_to_chatml(message: BaseMessage) -> ChatMLItem:
+    """Convert LangChain message to ChatML format."""
+
+    if isinstance(message, SystemMessage):
+        role = "system"
+    elif isinstance(message, AIMessage):
+        role = "assistant"
+    elif isinstance(message, HumanMessage):
+        role = "user"
+    else:
+        raise ValueError(f"Unknown message type: {type(message)}")
+
+    return {"role": role, "content": message.content}
+
+
+def get_completion_from_llmresponse(response: LLMResult) -> CompletionResponse:
+    response_dict = response.dict()
+
+    choice: ChoiceItem = {
+        "finish_reason": response_dict['generations'][0][0]['generation_info']['finish_reason'],
+        "index": 0,
+        "message": {
+            "role": "assistant",
+            "content": response_dict['generations'][0][0]['message']['content'],
+            "function_call": None,
+            "tool_calls": None
+        }
+    }
+
+    completion: CompletionResponse = {
+        'id': "",
+        'choices': [choice],
+        'model':  response_dict['llm_output']['model_name'],
+        'type': response_dict['generations'][0][0]['type'],
+        'system_fingerprint': response_dict['llm_output']['system_fingerprint'],
+        'usage': None
+    }
+
+    if response_dict['llm_output']['token_usage'] is not None:
+        usage: UsageItem = {
+            'completion_tokens': response_dict['llm_output']['token_usage']['completion_tokens'],
+            'prompt_tokens': response_dict['llm_output']['token_usage']['prompt_tokens'],
+            "total_tokens": response_dict['llm_output']['token_usage']['total_tokens']
+        }
+        completion['usage'] = usage
+
+    return completion
 
 
 class LangchainCallbackHandler(BaseCallbackHandler):
@@ -43,7 +114,40 @@ class LangchainCallbackHandler(BaseCallbackHandler):
         # keypair <run_id: str, Event>
         self.events: Dict[Any, Event] = {}
 
+    def on_chat_model_start(
+        self,
+        serialized: Dict[str, Any],
+        messages: List[List[BaseMessage]],
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[List[str]] = None,
+        metadata: Optional[Dict[str, Any]] = None,
+        **kwargs: Any,
+    ) -> Any:
+        """Run when a chat model starts running.
+
+        **ATTENTION**: This method is called for chat models. If you're implementing
+            a handler for a non-chat model, you should use on_llm_start instead.
+        """
+        try:
+            prompt = [message_to_chatml(message) for
+                      message in messages[0]]
+        except:
+            prompt = ''
+
+        self.events[run_id] = Event(
+            event_type="llm",
+            action_type='llm',
+            tags=tags,
+            model=kwargs['invocation_params']['model'],
+            params={**kwargs, **({} if metadata is None else metadata)},
+            prompt=prompt,
+            init_timestamp=get_ISO_time()
+        )
+
     # LLM Callbacks
+
     def on_llm_start(
         self,
         serialized: Dict[str, Any],
@@ -61,7 +165,7 @@ class LangchainCallbackHandler(BaseCallbackHandler):
             tags=tags,
             model=kwargs['invocation_params']['model'],
             params={**kwargs, **({} if metadata is None else metadata)},
-            prompt=prompts[0],
+            prompt=prompt_to_chatml(prompts[0]),
             init_timestamp=get_ISO_time()
         )
 
@@ -86,11 +190,11 @@ class LangchainCallbackHandler(BaseCallbackHandler):
         parent_run_id: Optional[UUID] = None,
         **kwargs: Any,
     ) -> Any:
+        completion = get_completion_from_llmresponse(response)
+        self.events[run_id].completion = completion
         self.events[run_id].end_timestamp = get_ISO_time()
-        self.events[run_id].returns = {
-            "content": response.generations[0][0].message.content,
-            "generations": response.generations
-        }
+        self.events[run_id].returns = response.dict()
+
         if response.llm_output is not None:
             self.events[run_id].prompt_tokens = response.llm_output['token_usage']['prompt_tokens']
             self.events[run_id].completion_tokens = response.llm_output['token_usage']['completion_tokens']
