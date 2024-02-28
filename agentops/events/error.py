085e79bddd7ff5d5915b3233e1edd5d1df68484f
Howard Gil <howardbgil@gmail.com>
2024-02-27 18:01:02 -0800

    WIP. Refactoring Event to an ABC

--- agentops/events/error.py
+++ agentops/events/error.py
@@ -1,13 +1,21 @@
-class Error(Event):
-    def __init__(self, session_id: str, event_id: int, event_type: str, type: str, code: str, details: str, logs: str, ...):
-        super().__init__(event_type='errors', result='Fail')
-        self.session_id = session_id
-        self.event_id = event_id
-        self.error_type = type
-        self.code = code
-        self.details = details
-        self.logs = logs
-        ...
-    
+from ..event import Event, EventType
+from typing import Optional
+
+
+class ErrorEvent(Event):
+    def __init__(self, error_type: Optional[str] = None, code: Optional[str] = None, details: Optional[str] = None, logs: Optional[str] = None, **kwargs):
+        super().__init__(event_type=EventType.error, **kwargs)
+        self.error_type = error_type  # TODO: type is a reserved word
+        self.code = code,
+        self.details = details,
+        self.logs = logs,
+
     def __str__(self):
-        return str({**super().__str__(), "session_id": self.session_id, "event_id": self.event_id, "type": self.error_type, ...})
+        super_str = super().__str__()
+        self_str = {
+            "error_type": self.error_type,
+            "code": self.code,
+            "details": self.details,
+            "logs": self.logs
+        }
+        return str({**super_str, **self_str})
