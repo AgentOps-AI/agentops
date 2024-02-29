2c85d196e74bbe8da4d9701de671b8c2eeb55846
Howard Gil <howardbgil@gmail.com>
2024-02-29 13:24:56 -0800

    WIP. Refactoring Event to an ABC

--- agentops/events/action.py
+++ agentops/events/action.py
@@ -2,10 +2,12 @@ from ..event import Event, EventType
 from typing import Optional
 from uuid import UUID
 
+# TODO: Remove agent_id because it's set by server?
+
 
 class ActionEvent(Event):
-    def __init__(self, agent_id: UUID, action_type: Optional[str] = None, detail: Optional[str] = None, logs: Optional[str] = None, screenshot: Optional[str] = None, **kwargs):
-        super().__init__(event_type=EventType.action, **kwargs)
+    def __init__(self, agent_id: Optional[UUID] = None, action_type: Optional[str] = None, detail: Optional[str] = None, logs: Optional[str] = None, screenshot: Optional[str] = None, **kwargs):
+        super().__init__(event_type=EventType.ACTION, **kwargs)
         self.agent_id = agent_id
         self.action_type = action_type
         self.detail = detail
