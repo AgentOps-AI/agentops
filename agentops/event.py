44fdab6479feee6af067bc6c43e2772541560f6c
Howard Gil <howardbgil@gmail.com>
2024-03-01 02:40:48 -0800

    Events working end to end MVP

--- agentops/event.py
+++ agentops/event.py
@@ -1,33 +1,66 @@
 """
 AgentOps events.
 
-Classes:
+Data Class:
     Event: Represents discrete events to be recorded.
 """
-from abc import ABC, abstractmethod
+
+from dataclasses import dataclass, field
+from typing import List, Optional
 from .helpers import get_ISO_time
-from typing import Optional, List
-from .enums import EventType
+from .enums import EventType, Models
+from uuid import UUID
 
 
-class Event(ABC):
-    """
-    Represents a discrete event to be recorded.
-    """
+@dataclass
+class Event:
+    event_type: str  # EventType.ENUM.value
+    tags: Optional[List[str]] = None
+    init_timestamp: Optional[str] = field(default_factory=get_ISO_time)
+    end_timestamp: str = field(default_factory=get_ISO_time)
 
-    def __init__(self, event_type: EventType = EventType.ACTION,
-                 tags: Optional[List[str]] = None,
-                 init_timestamp: Optional[str] = None,
-                 ):
-        self.event_type = event_type
-        self.tags = tags
-        self.end_timestamp = get_ISO_time()
-        self.init_timestamp = init_timestamp if init_timestamp else self.end_timestamp
 
-    @abstractmethod
-    def __str__(self):
-        return str({
-            "event_type": self.event_type,
-            "tags": self.tags,
-            "init_timestamp": self.init_timestamp,
-        })
+@dataclass
+class ActionEvent(Event):
+    event_type: str = EventType.ACTION.value
+    # TODO: Should not be optional, but non-default argument 'agent_id' follows default argument error
+    agent_id: Optional[UUID] = None
+    action_type: Optional[str] = None
+    detail: Optional[str] = None
+    logs: Optional[str] = None
+    screenshot: Optional[str] = None
+
+    # May be needed if we keep Optional for agent_id
+    # def __post_init__(self):
+    #     if self.agent_id is None:
+    #         raise ValueError("agent_id is required for ActionEvent")
+
+
+@dataclass
+class ErrorEvent(Event):
+    event_type: str = EventType.ERROR.value
+    # Note: 'type' is a reserved keyword in Python, hence 'error_type'
+    error_type: Optional[str] = None
+    code: Optional[str] = None
+    details: Optional[str] = None
+    logs: Optional[str] = None
+
+
+@dataclass
+class LLMEvent(Event):
+    event_type: str = EventType.LLM.value
+    thread_id: Optional[UUID] = None
+    model: Optional[Models] = None
+    prompt: Optional[str] = None
+    prompt_tokens: Optional[int] = None
+    completion_tokens: Optional[int] = None
+
+
+@dataclass
+class ToolEvent(Event):
+    event_type: str = EventType.TOOL.value
+    agent_id: Optional[UUID] = None
+    name: Optional[str] = None
+    inputs: Optional[str] = None
+    outputs: Optional[str] = None
+    logs: Optional[str] = None
