c3feab43db42b65f5d606e8cd296ef345280935b
Howard Gil <howardbgil@gmail.com>
2024-03-04 18:14:00 -0800

    removed comment

--- agentops/__init__.py
+++ agentops/__init__.py
@@ -1,7 +1,6 @@
 # agentops/__init__.py
 
 from .client import Client
-# TODO: Don't expose Event since it's abstract?
 from .event import ActionEvent, ErrorEvent, LLMEvent, ToolEvent
 from .logger import AgentOpsLogger
 from .enums import Models
