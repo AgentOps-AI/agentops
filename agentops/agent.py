fd244823cc07ef392e9b663b12ffe712dfc65c7f
Braelyn Boynton <bboynton97@gmail.com>
2024-03-07 17:30:55 -0800

    agent instances have unique ids

--- agentops/agent.py
+++ agentops/agent.py
@@ -5,12 +5,17 @@ from agentops import Client
 def track_agent(name: str | None):
     def class_decorator(cls):
         cls._is_ao_agent = True
-        cls._ao_agent_id = str(uuid.uuid4())
         cls._ao_agent_name = name or cls.__name__
 
-        # create agent record
-        ao_client = Client()
-        ao_client.create_agent(cls._ao_agent_id, cls._ao_agent_name)
+        original_init = cls.__init__
+
+        def new_init(self, *args, **kwargs):
+            self._ao_agent_id = str(uuid.uuid4())
+            ao_client = Client()
+            ao_client.create_agent(self._ao_agent_id, self._ao_agent_name)
+            original_init(self, *args, **kwargs)
+
+        cls.__init__ = new_init
         return cls
 
     return class_decorator
