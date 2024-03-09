592c14a04d7570bbcc40ac37385c19804f77180b
Braelyn Boynton <bboynton97@gmail.com>
2024-03-08 16:44:45 -0800

    supports functions

--- agentops/agent.py
+++ agentops/agent.py
@@ -1,20 +1,31 @@
 import uuid
 from agentops import Client
+from inspect import isclass, isfunction
 
 
 def track_agent(name: str | None):
-    def class_decorator(cls):
-        cls._agent_ops_agent_name = name or cls.__name__
+    def decorator(obj):
+        obj._agent_ops_agent_name = name or obj.__name__
 
-        original_init = cls.__init__
+        if isclass(obj):
+            original_init = obj.__init__
 
-        def new_init(self, *args, **kwargs):
-            self._agent_ops_agent_id = str(uuid.uuid4())
+            def new_init(self, *args, **kwargs):
+                self._agent_ops_agent_id = str(uuid.uuid4())
+                ao_client = Client()
+                ao_client.create_agent(self._agent_ops_agent_id, self._agent_ops_agent_name)
+                original_init(self, *args, **kwargs)
+
+            obj.__init__ = new_init
+
+        elif isfunction(obj):
+            obj._agent_ops_agent_id = str(uuid.uuid4())
             ao_client = Client()
-            ao_client.create_agent(self._agent_ops_agent_id, self._agent_ops_agent_name)
-            original_init(self, *args, **kwargs)
+            ao_client.create_agent(obj._agent_ops_agent_id, obj._agent_ops_agent_name)
 
-        cls.__init__ = new_init
-        return cls
+        else:
+            raise Exception("Invalid input, 'obj' must be a class or a function")
 
-    return class_decorator
+        return obj
+
+    return decorator
