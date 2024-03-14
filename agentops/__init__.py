3aef3d405996793836ecd5aeed01910d45cd7393
Howard Gil <howardbgil@gmail.com>
2024-03-13 18:24:43 -0700

    Refactored for Errors

--- agentops/__init__.py
+++ agentops/__init__.py
@@ -1,6 +1,6 @@
 # agentops/__init__.py
 
 from .client import Client
-from .event import ActionEvent, ErrorEvent, LLMEvent, ToolEvent
+from .event import ActionEvent, LLMEvent, ToolEvent, Error
 from .logger import AgentOpsLogger
 from .enums import Models
--- agentops/event.py
+++ agentops/event.py
@@ -9,25 +9,30 @@ from dataclasses import dataclass, field
 from typing import List, Optional
 from .helpers import get_ISO_time
 from .enums import EventType, Models
-from uuid import UUID
+from uuid import uuid4
 
 
 @dataclass
 class Event:
     event_type: str  # EventType.ENUM.value
     tags: Optional[List[str]] = None
-    params: Optional[str] = None  # TODO: We should add params to all tables in supabase, right now it's just LLMEvent
+    params: Optional[str] = None
+    returns: Optional[str] = None
     init_timestamp: Optional[str] = field(default_factory=get_ISO_time)
     end_timestamp: str = field(default_factory=get_ISO_time)
+    # TODO: instead of making this a str we can keep it as uuid4 and just handle it on serialization
+    # e.g. if isinstance(o, UUID): return str(o)
+    # we might have to do this bc when they start passing uuid4 it'll error unless they do str(uuid4())
+    # this is why i have to use lambda
+    id: str = field(default_factory=lambda: str(uuid4()))
 
 
 @dataclass
 class ActionEvent(Event):
     event_type: str = EventType.ACTION.value
     # TODO: Should not be optional, but non-default argument 'agent_id' follows default argument error
-    agent_id: Optional[UUID] = None
+    agent_id: Optional[uuid4] = None
     action_type: Optional[str] = None
-    details: Optional[str] = None
     logs: Optional[str] = None
     screenshot: Optional[str] = None
 
@@ -38,21 +43,10 @@ class ActionEvent(Event):
 
 
 @dataclass
-class ErrorEvent(Event):
-    event_type: str = EventType.ERROR.value
-    event_name: Optional[str] = None
-    error_type: Optional[str] = None
-    code: Optional[str] = None
-    details: Optional[str] = None
-    logs: Optional[str] = None
-
-
-@dataclass
 class LLMEvent(Event):
     event_type: str = EventType.LLM.value
-    params: Optional[str] = None
-    agent_id: Optional[UUID] = None
-    thread_id: Optional[UUID] = None
+    agent_id: Optional[uuid4] = None
+    thread_id: Optional[uuid4] = None
     prompt: Optional[str] = None
     completion: Optional[str] = None
     model: Optional[Models] = None
@@ -63,8 +57,24 @@ class LLMEvent(Event):
 @dataclass
 class ToolEvent(Event):
     event_type: str = EventType.TOOL.value
-    agent_id: Optional[UUID] = None
+    agent_id: Optional[uuid4] = None
     name: Optional[str] = None
-    inputs: Optional[str] = None
-    outputs: Optional[str] = None
     logs: Optional[str] = None
+
+
+####################
+@dataclass
+class Error():
+    trigger_event_id: Optional[uuid4]
+    trigger_event_type: Optional[EventType]
+    error_type: Optional[str] = None
+    code: Optional[str] = None
+    details: Optional[str] = None
+    logs: Optional[str] = None
+    timestamp: str = field(default_factory=get_ISO_time)
+
+    def __init__(self, event: Event, **kwargs):
+        self.trigger_event_id = event.id
+        self.trigger_event_type = event.event_type
+        for key, value in kwargs.items():
+            setattr(self, key, value)
