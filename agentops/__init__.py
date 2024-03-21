1337d0eba104154c2c8a3379cd85492935d91975
Braelyn Boynton <bboynton97@gmail.com>
2024-03-21 12:57:31 -0700

    use init with singleton

--- agentops/__init__.py
+++ agentops/__init__.py
@@ -5,3 +5,19 @@ from .event import ActionEvent, LLMEvent, ToolEvent, ErrorEvent
 from .logger import AgentOpsLogger
 from .enums import Models, LLMMessageFormat
 from .decorators import record_function
+
+
+def init(*args, **kwargs):
+    Client(*args, **kwargs)
+
+
+def end_session(*args, **kwargs):
+    Client().end_session(*args, **kwargs)
+
+
+def start_session(*args, **kwargs):
+    Client().start_session(*args, **kwargs)
+
+
+def record(*args, **kwargs):
+    Client().record(*args, **kwargs)
