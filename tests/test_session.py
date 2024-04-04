66f8c0e3e7805894081b897a6c646a359e9fa226
Shawn Qiu <siyangqiu@gmail.com>
2024-04-04 16:02:56 -0700

    actually looser timings

--- tests/test_session.py
+++ tests/test_session.py
@@ -18,17 +18,17 @@ class TestSessions:
     def setup_method(self):
         self.api_key = "random_api_key"
         self.event_type = 'test_event_type'
-        agentops.init(api_key=self.api_key, max_wait_time=200, auto_start_session=False)
+        self.config = agentops.Configuration(api_key=self.api_key, max_wait_time=50)
 
     def test_session(self, mock_req):
-        agentops.start_session()
+        agentops.start_session(config=self.config)
 
         agentops.record(ActionEvent(self.event_type))
         agentops.record(ActionEvent(self.event_type))
 
-        # We should have 1 requests (session start). The action events should still be in queue
+        # We should have 1 requests (session start).
         assert len(mock_req.request_history) == 1
-        time.sleep(0.5)
+        time.sleep(2)
 
         # We should have 2 requests (session and 2 events combined into 1)
         print(mock_req.last_request.json())
@@ -39,7 +39,7 @@ class TestSessions:
 
         end_state = 'Success'
         agentops.end_session(end_state)
-        time.sleep(0.5)
+        time.sleep(0.2)
 
         # We should have 3 requests (additional end session)
         assert len(mock_req.request_history) == 3
@@ -51,11 +51,11 @@ class TestSessions:
     def test_tags(self, mock_req):
         # Arrange
         tags = ['GPT-4']
-        agentops.start_session(tags=tags)
+        agentops.start_session(tags=tags, config=self.config)
 
         # Act
         agentops.record(ActionEvent(self.event_type))
-        time.sleep(0.5)
+        time.sleep(0.2)
 
         # Assert 2 requests - 1 for session init, 1 for event
         print(mock_req.last_request.json())
