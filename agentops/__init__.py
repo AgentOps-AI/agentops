bdb7a412e68226909e858cb39f2530006a804cda
Howard Gil <howardbgil@gmail.com>
2024-04-25 19:22:02 -0700

    Instrument litellm (#163)
    * Setting session to none if server does not return 200 for /sessions
    
    * WIP. Changing override logic
    
    * Working
    
    * Tidying
    
    * WIP
    
    * Finished litellm
    
    * Added back override with deprecation message. Docstring fixes
    
    * Indent
    
    * Updated minimum supported litellm version
    
    * syntax

--- agentops/__init__.py
+++ agentops/__init__.py
@@ -17,7 +17,8 @@ def init(api_key: Optional[str] = None,
          max_wait_time: Optional[int] = None,
          max_queue_size: Optional[int] = None,
          tags: Optional[List[str]] = None,
-         override=True,
+         override: Optional[bool] = None,  # Deprecated
+         instrument_llm_calls=True,
          auto_start_session=True,
          inherited_session_id: Optional[str] = None
          ):
@@ -37,11 +38,12 @@ def init(api_key: Optional[str] = None,
             max_queue_size (int, optional): The maximum size of the event queue. Defaults to 100.
             tags (List[str], optional): Tags for the sessions that can be used for grouping or
                 sorting later (e.g. ["GPT-4"]).
-            override (bool): Whether to override and LLM calls to emit as events.
+            override (bool, optional): [Deprecated] Use `instrument_llm_calls` instead. Whether to instrument LLM calls and emit LLMEvents..
+            instrument_llm_calls (bool): Whether to instrument LLM calls and emit LLMEvents..
             auto_start_session (bool): Whether to start a session automatically when the client is created.
             inherited_session_id (optional, str): Init Agentops with an existing Session
         Attributes:
-        """
+    """
     set_logging_level_info()
     c = Client(api_key=api_key,
                parent_key=parent_key,
@@ -50,32 +52,67 @@ def init(api_key: Optional[str] = None,
                max_queue_size=max_queue_size,
                tags=tags,
                override=override,
+               instrument_llm_calls=instrument_llm_calls,
                auto_start_session=auto_start_session,
                inherited_session_id=inherited_session_id
                )
-    
+
     return inherited_session_id or c.current_session_id
 
 
 def end_session(end_state: str,
                 end_state_reason: Optional[str] = None,
                 video: Optional[str] = None):
+    """
+        End the current session with the AgentOps service.
+
+        Args:
+            end_state (str): The final state of the session. Options: Success, Fail, or Indeterminate.
+            end_state_reason (str, optional): The reason for ending the session.
+            video (str, optional): URL to a video recording of the session
+    """
     Client().end_session(end_state, end_state_reason, video)
 
 
 def start_session(tags: Optional[List[str]] = None, config: Optional[Configuration] = None, inherited_session_id: Optional[str] = None):
+    """
+        Start a new session for recording events.
+
+        Args:
+            tags (List[str], optional): Tags that can be used for grouping or sorting later.
+                e.g. ["test_run"].
+            config: (Configuration, optional): Client configuration object
+    """
     return Client().start_session(tags, config, inherited_session_id)
 
 
 def record(event: Event | ErrorEvent):
+    """
+        Record an event with the AgentOps service.
+
+        Args:
+            event (Event): The event to record.
+    """
     Client().record(event)
 
 
 def add_tags(tags: List[str]):
+    """
+        Append to session tags at runtime. 
+
+        Args:
+            tags (List[str]): The list of tags to append.
+    """
     Client().add_tags(tags)
 
 
 def set_tags(tags: List[str]):
+    """
+        Replace session tags at runtime. 
+
+        Args:
+            tags (List[str]): The list of tags to set.
+    """
     Client().set_tags(tags)
 
 
@@ -84,4 +121,10 @@ def get_api_key() -> str:
 
 
 def set_parent_key(parent_key):
+    """
+        Set the parent API key which has visibility to projects it is parent to.
+
+        Args:
+            parent_key (str): The API key of the parent organization to set.
+    """
     Client().set_parent_key(parent_key)
