dd47790051fb5f1b330e23675da792ec085de12e
Howard Gil <howardbgil@gmail.com>
2024-05-16 03:42:43 -0700

    Better example

--- examples/cohere_example.py
+++ examples/cohere_example.py
@@ -6,21 +6,6 @@ load_dotenv()
 agentops.init(tags=["cohere"])
 co = cohere.Client()
 
-# stream = co.chat_stream(
-#     message="What three lines do I need to add to my code to integrate AgentOps?",
-#     connectors=[{"id": "web-search", "options": {"site": "https://docs.agentops.ai/v1/quickstart"}}]
-# )
-
-# for event in stream:
-#     if event.event_type == "text-generation":
-#         print(event.text, end='')
-
-
-# stream = co.chat_stream(
-#     message="Does crew support AgentOps?",
-#     connectors=[{"id": "web-search", "options": {"site": "https://www.crewai.com/"}}]
-# )
-
 stream = co.chat_stream(
     message="Tell me everything you can about AgentOps",
     connectors=[{"id": "web-search"}]
@@ -32,6 +17,8 @@ for event in stream:
         response += event.text
         print(event.text, end='')
 
+print("\n")
+print(stream)
 print("\n")
 
 stream = co.chat_stream(
@@ -53,19 +40,8 @@ for event in stream:
         print(event.text, end='')
 
 print("\n")
+print(stream)
+print("\n")
 
-# chat = co.chat(
-#     chat_history=[
-#         {"role": "SYSTEM", "message": "You are Adam Silverman: die-hard advocate of AgentOps, leader in AI Agent observability"},
-#         {
-#             "role": "CHATBOT",
-#             "message": "How's your day going?",
-#         },
-#     ],
-#     message="Great! Cohere is looking for an observability partner, know of any good ones?",
-#     connectors=[{"id": "web-search", "options": {"site": "https://docs.agentops.ai/v1/quickstart"}}]
-# )
-
-# print(chat)
 
 agentops.end_session('Success')
