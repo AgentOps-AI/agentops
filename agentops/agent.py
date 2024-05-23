c628ee0bc62ddeac9e88842fcab17a975b913603
Braelyn Boynton <bboynton97@gmail.com>
2024-05-22 20:03:39 -0700

    merge

--- agentops/agent.py
+++ agentops/agent.py
@@ -20,7 +20,7 @@ def track_agent(name: Union[str, None] = None):
                     self.agent_ops_agent_id = str(uuid4())
                     Client().create_agent(name=self.agent_ops_agent_name, agent_id=self.agent_ops_agent_id)
                 except AttributeError as e:
-                    logger.warning("AgentOps failed to track an agent. This often happens if agentops.init() was not "
+                    logger.warning("Failed to track an agent. This often happens if agentops.init() was not "
                                    "called before initializing an agent with the @track_agent decorator.")
                     raise e
 
@@ -31,7 +31,8 @@ def track_agent(name: Union[str, None] = None):
             Client().create_agent(name=obj.agent_ops_agent_name, agent_id=obj.agent_ops_agent_id)
 
         else:
-            raise Exception("Invalid input, 'obj' must be a class or a function")
+            raise Exception(
+                "Invalid input, 'obj' must be a class or a function")
 
         return obj
 
