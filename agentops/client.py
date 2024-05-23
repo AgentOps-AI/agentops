3893555299500e564f732282747e75f4d27d5907
Braelyn Boynton <bboynton97@gmail.com>
2024-05-23 16:23:30 -0700

    remove todos

--- agentops/client.py
+++ agentops/client.py
@@ -119,8 +119,7 @@ class Client(metaclass=MetaClient):
                 if framework == 'autogen':
                     try:
                         import autogen
-                        from .partners.autogen_logger import AutogenLogger  # TODO: move?
-                        # TODO: can we pass it like this without it being a singleton?
+                        from .partners.autogen_logger import AutogenLogger
                         autogen.runtime_logging.start(logger=AutogenLogger())
                     except ImportError:
                         pass
