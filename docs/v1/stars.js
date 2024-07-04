1f925feebb4b437432e49e6b876ccfdb80a19ed9
Soo <soodonim@theplaytest.com>
2024-07-04 11:25:36 -0700

    Rounds up to nearest 100, makes script elegante
    suck it mint growth plan, I did it my damn self

---
+++ docs/v1/stars.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() {
+  fetch("https://api.github.com/repos/AgentOps-AI/agentops")
+    .then((response) => response.json())
+    .then((data) => {
+      const stars = Math.ceil(data.stargazers_count / 100) * 100 + 100;
+      const dataContainer = document.getElementById("stars-text");
+      dataContainer.innerHTML = `${stars.toLocaleString()}th`;
+
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      document.getElementById("stars-text").textContent = 'Error loading data.';
+    });
+})
