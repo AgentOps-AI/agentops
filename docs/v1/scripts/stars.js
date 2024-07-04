efea4442e4bb303b3ad4564f81f0c43fd361a511
Soo <soodonim@theplaytest.com>
2024-07-04 15:27:13 -0700

    API Key dynamic (pending endpoint)
    Fixes #272

--- docs/v1/scripts/stars.js
+++ docs/v1/scripts/stars.js
@@ -3,26 +3,28 @@ function updateStars() {
     .then((response) => response.json())
     .then((data) => {
       const stars = Math.ceil(data.stargazers_count / 100) * 100 + 100;
-      setTimeout(() => {
-        const dataContainer = document.getElementById("stars-text");
-        dataContainer.innerHTML = `${stars.toLocaleString()}th`;
-      }, 50);
+      const dataContainer = document.getElementById("stars-text");
+      dataContainer.innerHTML = `${stars.toLocaleString()}th`;
     })
     .catch((error) => {
       // console.error("Github Stars pull error:", error);
     });
 }
 
-function addNewEventListeners() {
+
+// Note that functions cannot be named the same thing across script files if copy-pasting this to add new scripts
+function addStarsEventListeners() {
   setTimeout(() => {
     document.querySelectorAll('li, a').forEach(element => {
-      element.addEventListener('click', updateStars);
+      element.addEventListener('click', () => {
+        setTimeout(updateStars, 50);
+      });
     });
   }, 50);
 }
 
 window.addEventListener('load', function() {
   updateStars();
-  const observer = new MutationObserver(addNewEventListeners);
-  observer.observe(document.body, { childList: true, subtree: true });
+  const starsObserver = new MutationObserver(addStarsEventListeners);
+  starsObserver.observe(document.body, { childList: true, subtree: true });
 });
