51c91b0527c9dce919ec8ddf84717f131e0846ba
Soo <soodonim@theplaytest.com>
2024-07-04 12:33:08 -0700

    also handle page navs and still update data

--- docs/v1/stars.js
+++ docs/v1/stars.js
@@ -1,13 +1,19 @@
-window.addEventListener('load', function() {
+function updateStars() {
   fetch("https://api.github.com/repos/AgentOps-AI/agentops")
     .then((response) => response.json())
     .then((data) => {
       const stars = Math.ceil(data.stargazers_count / 100) * 100 + 100;
       const dataContainer = document.getElementById("stars-text");
       dataContainer.innerHTML = `${stars.toLocaleString()}th`;
-
     })
     .catch((error) => {
-      console.error("Error:", error);
+      // console.error("Github Stars pull error:", error);
     });
-})
+}
+
+window.addEventListener('load', function() {
+  updateStars();
+  document.querySelectorAll('li, a').forEach(item => {
+    item.addEventListener('click', updateStars);
+  });
+});
