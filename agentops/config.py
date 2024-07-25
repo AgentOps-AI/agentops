b9fdd903c22d6e4e279710aaba767fdc4f8658da
Shawn Qiu <siyangqiu@gmail.com>
2024-07-24 23:50:02 -0700

    code cleanup

--- agentops/config.py
+++ agentops/config.py
@@ -19,31 +19,6 @@ class Configuration:
         skip_auto_end_session: Optional[bool] = None,
         env_data_opt_out: Optional[bool] = None,
     ):
-        # Check if default_tags is actually a list. If it is a single string, convert it to a list
-        if isinstance(default_tags, str):
-            default_tags = [default_tags]
-        if not isinstance(default_tags, list):
-            logger.warning(
-                "default_tags is not a list - default tags will be not set. Please pass a List to default_tags"
-            )
-
-        self.api_key: Optional[str] = None
-        self.parent_key: Optional[str] = None
-        self.endpoint: str = (
-            endpoint
-            or os.environ.get("AGENTOPS_API_ENDPOINT")
-            or "https://api.agentops.ai"
-        )
-        self.max_wait_time: int = max_wait_time or 5000
-        self.max_queue_size: int = max_queue_size or 100
-        self.default_tags: List[str] = default_tags or []
-        self.instrument_llm_calls: bool = instrument_llm_calls or True
-        self.auto_start_session: bool = auto_start_session or True
-        self.skip_auto_end_session: bool = skip_auto_end_session or False
-        self.env_data_opt_out: bool = (
-            env_data_opt_out
-            or os.environ.get("AGENTOPS_ENV_DATA_OPT_OUT", "False").lower() == "true"
-        )
 
         api_key = api_key or os.environ.get("AGENTOPS_API_KEY")
         if api_key is not None:
@@ -60,3 +35,29 @@ class Configuration:
                 self.parent_key = parent_key
             except ValueError:
                 logger.warning(f"Parent Key is invalid: {parent_key}")
+
+        # Check if default_tags is actually a list. If it is a single string, convert it to a list
+        if isinstance(default_tags, str):
+            default_tags = [default_tags]
+        if not isinstance(default_tags, list):
+            logger.warning(
+                "default_tags is not a list - default tags will be not set. Please pass a List to default_tags"
+            )
+
+        self.api_key: Optional[str] = api_key
+        self.parent_key: Optional[str] = parent_key
+        self.endpoint: str = (
+            endpoint
+            or os.environ.get("AGENTOPS_API_ENDPOINT")
+            or "https://api.agentops.ai"
+        )
+        self.max_wait_time: int = max_wait_time or 5000
+        self.max_queue_size: int = max_queue_size or 100
+        self.default_tags: List[str] = default_tags or []
+        self.instrument_llm_calls: bool = instrument_llm_calls or True
+        self.auto_start_session: bool = auto_start_session or True
+        self.skip_auto_end_session: bool = skip_auto_end_session or False
+        self.env_data_opt_out: bool = (
+            env_data_opt_out
+            or os.environ.get("AGENTOPS_ENV_DATA_OPT_OUT", "False").lower() == "true"
+        )
