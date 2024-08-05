8821ce2fde98a7ce2d966398f47951a55cca50a7
Braelyn Boynton <bboynton97@gmail.com>
2024-08-05 13:40:50 -0700

    remove shared code, fix tests

--- tests/test_record_tool.py
+++ tests/test_record_tool.py
@@ -40,14 +40,14 @@ def mock_req():
         yield m
 
 
-class TestRecordAction:
+class TestRecordTool:
     def setup_method(self):
         self.url = "https://api.agentops.ai"
         self.api_key = "11111111-1111-4111-8111-111111111111"
         self.tool_name = "test_tool_name"
         agentops.init(self.api_key, max_wait_time=5, auto_start_session=False)
 
-    def test_record_function_decorator(self, mock_req):
+    def test_record_tool_decorator(self, mock_req):
         agentops.start_session()
 
         @record_tool(tool_name=self.tool_name)
@@ -62,13 +62,13 @@ class TestRecordAction:
         assert len(mock_req.request_history) == 2
         assert mock_req.last_request.headers["X-Agentops-Api-Key"] == self.api_key
         request_json = mock_req.last_request.json()
-        assert request_json["events"][0]["tool_name"] == self.tool_name
+        assert request_json["events"][0]["name"] == self.tool_name
         assert request_json["events"][0]["params"] == {"x": 3, "y": 4}
         assert request_json["events"][0]["returns"] == 7
 
         agentops.end_session(end_state="Success")
 
-    def test_record_function_decorator_multiple(self, mock_req):
+    def test_record_tool_decorator_multiple(self, mock_req):
         agentops.start_session()
 
         # Arrange
@@ -86,17 +86,17 @@ class TestRecordAction:
         assert len(mock_req.request_history) == 3
         assert mock_req.last_request.headers["X-Agentops-Api-Key"] == self.api_key
         request_json = mock_req.last_request.json()
-        assert request_json["events"][0]["tool_name"] == self.tool_name
+        assert request_json["events"][0]["name"] == self.tool_name
         assert request_json["events"][0]["params"] == {"x": 1, "y": 2, "z": 3}
         assert request_json["events"][0]["returns"] == 6
 
         agentops.end_session(end_state="Success")
 
     @pytest.mark.asyncio
-    async def test_async_function_call(self, mock_req):
+    async def test_async_tool_call(self, mock_req):
         agentops.start_session()
 
-        @record_function(self.tool_name)
+        @record_tool(self.tool_name)
         async def async_add(x, y):
             time.sleep(0.1)
             return x + y
@@ -111,7 +111,7 @@ class TestRecordAction:
         assert len(mock_req.request_history) == 2
         assert mock_req.last_request.headers["X-Agentops-Api-Key"] == self.api_key
         request_json = mock_req.last_request.json()
-        assert request_json["events"][0]["tool_name"] == self.tool_name
+        assert request_json["events"][0]["name"] == self.tool_name
         assert request_json["events"][0]["params"] == {"x": 3, "y": 4}
         assert request_json["events"][0]["returns"] == 7
 
@@ -145,7 +145,7 @@ class TestRecordAction:
         request_json = mock_req.last_request.json()
         assert mock_req.last_request.headers["X-Agentops-Api-Key"] == self.api_key
         assert mock_req.last_request.headers["Authorization"] == f"Bearer some_jwt2"
-        assert request_json["events"][0]["tool_name"] == self.tool_name
+        assert request_json["events"][0]["name"] == self.tool_name
         assert request_json["events"][0]["params"] == {"x": 1, "y": 2, "z": 3}
         assert request_json["events"][0]["returns"] == 6
 
@@ -156,7 +156,7 @@ class TestRecordAction:
         assert (
             mock_req.request_history[-2].headers["Authorization"] == f"Bearer some_jwt"
         )
-        assert second_last_request_json["events"][0]["tool_name"] == self.tool_name
+        assert second_last_request_json["events"][0]["name"] == self.tool_name
         assert second_last_request_json["events"][0]["params"] == {
             "x": 1,
             "y": 2,
@@ -192,7 +192,7 @@ class TestRecordAction:
         request_json = mock_req.last_request.json()
         assert mock_req.last_request.headers["X-Agentops-Api-Key"] == self.api_key
         assert mock_req.last_request.headers["Authorization"] == f"Bearer some_jwt2"
-        assert request_json["events"][0]["tool_name"] == self.tool_name
+        assert request_json["events"][0]["name"] == self.tool_name
         assert request_json["events"][0]["params"] == {"x": 1, "y": 2}
         assert request_json["events"][0]["returns"] == 3
 
@@ -203,7 +203,7 @@ class TestRecordAction:
         assert (
             mock_req.request_history[-2].headers["Authorization"] == f"Bearer some_jwt"
         )
-        assert second_last_request_json["events"][0]["tool_name"] == self.tool_name
+        assert second_last_request_json["events"][0]["name"] == self.tool_name
         assert second_last_request_json["events"][0]["params"] == {
             "x": 1,
             "y": 2,
