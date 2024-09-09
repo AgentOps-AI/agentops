1edfe3818a6e4afbe5c1804d443278dbc20e814c
Braelyn Boynton <bboynton97@gmail.com>
2024-09-09 12:26:41 +0900

    remove comments

--- agentops/decorators.py
+++ agentops/decorators.py
@@ -326,12 +326,6 @@ def track_agent(name: Union[str, None] = None):
 
                     original_init(self, *args, **kwargs)
 
-                    # if not Client().is_initialized:
-                    #     Client().add_pre_init_warning(
-                    #         f"Failed to track an agent {name} because agentops.init() was not "
-                    #         + "called before initializing the agent with the @track_agent decorator."
-                    #     )
-
                     self.agent_ops_agent_id = str(uuid4())
 
                     session = kwargs.get("session", None)
@@ -345,14 +339,6 @@ def track_agent(name: Union[str, None] = None):
                     )
                 except AttributeError as e:
                     print(e)
-                    # Client().add_pre_init_warning(
-                    #     f"Failed to track an agent {name} because agentops.init() was not "
-                    #     + "called before initializing the agent with the @track_agent decorator."
-                    # )
-                    # logger.warning(
-                    #     "Failed to track an agent. This often happens if agentops.init() was not "
-                    #     "called before initializing an agent with the @track_agent decorator."
-                    # )
                     original_init(self, *args, **kwargs)
 
             obj.__init__ = new_init
