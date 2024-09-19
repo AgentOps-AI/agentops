765a0fa6a0321381eba6cdc9a0d86eb28b3f4414
Pratyush Shukla <ps4534@nyu.edu>
2024-09-19 20:46:35 +0530

    stream not working

--- agentops/llms/ai21.py
+++ agentops/llms/ai21.py
@@ -1,3 +1,4 @@
+import inspect
 import pprint
 from typing import Optional
 
@@ -16,8 +17,8 @@ class AI21Provider(InstrumentedProvider):
 
     original_create = None
     original_create_async = None
-    original_stream = None
-    original_stream_async = None
+    # original_stream = None
+    # original_stream_async = None
 
     def __init__(self, client):
         super().__init__(client)
@@ -27,66 +28,91 @@ class AI21Provider(InstrumentedProvider):
         self, response, kwargs, init_timestamp, session: Optional[Session] = None
     ):
         """Handle responses for AI21"""
+        from ai21.stream.stream import Stream
+        from ai21.stream.async_stream import AsyncStream
+        from ai21.models.chat.chat_completion_chunk import ChatCompletionChunk
 
         llm_event = LLMEvent(init_timestamp=init_timestamp, params=kwargs)
         if session is not None:
             llm_event.session_id = session.session_id
 
-        # def handle_stream_chunk(chunk: dict):
-        #     # We take the first ChatCompletionChunk and accumulate the deltas from all subsequent chunks to build one full chat completion
-        #     if llm_event.returns is None:
-        #         llm_event.returns = chunk.data
+        def handle_stream_chunk(chunk: ChatCompletionChunk):
+            # We take the first ChatCompletionChunk and accumulate the deltas from all subsequent chunks to build one full chat completion
+            if llm_event.returns is None:
+                llm_event.returns = chunk
 
-        #     try:
-        #         accumulated_delta = llm_event.returns.choices[0].delta
-        #         llm_event.agent_id = check_call_stack_for_agent_id()
-        #         llm_event.model = chunk.data.model
-        #         llm_event.prompt = kwargs["messages"]
+            try:
+                accumulated_delta = llm_event.returns.choices[0].delta
+                llm_event.agent_id = check_call_stack_for_agent_id()
+                llm_event.model = kwargs["model"]
+                llm_event.prompt = [message.model_dump() for message in kwargs["messages"]]
 
-        #         # NOTE: We assume for completion only choices[0] is relevant
-        #         choice = chunk.data.choices[0]
+                # NOTE: We assume for completion only choices[0] is relevant
+                choice = chunk.choices[0]
 
-        #         if choice.delta.content:
-        #             accumulated_delta.content += choice.delta.content
+                if choice.delta.content:
+                    accumulated_delta.content += choice.delta.content
 
-        #         if choice.delta.role:
-        #             accumulated_delta.role = choice.delta.role
+                if choice.delta.role:
+                    accumulated_delta.role = choice.delta.role
 
-        #         # Check if tool_calls is Unset and set to None if it is
-        #         if choice.delta.tool_calls in (UNSET, UNSET_SENTINEL):
-        #             accumulated_delta.tool_calls = None
-        #         elif choice.delta.tool_calls:
-        #             accumulated_delta.tool_calls = choice.delta.tool_calls
+                if choice.finish_reason:
+                    # Streaming is done. Record LLMEvent
+                    llm_event.returns.choices[0].finish_reason = (
+                        choice.finish_reason
+                    )
+                    llm_event.completion = {
+                        "role": accumulated_delta.role,
+                        "content": accumulated_delta.content,
+                    }
+                    llm_event.prompt_tokens = chunk.usage.prompt_tokens
+                    llm_event.completion_tokens = chunk.usage.completion_tokens
+                    llm_event.end_timestamp = get_ISO_time()
+                    self._safe_record(session, llm_event)
 
-        #         if chunk.data.choices[0].finish_reason:
-        #             # Streaming is done. Record LLMEvent
-        #             llm_event.returns.choices[0].finish_reason = (
-        #                 choice.finish_reason
-        #             )
-        #             llm_event.completion = {
-        #                 "role": accumulated_delta.role,
-        #                 "content": accumulated_delta.content,
-        #                 "tool_calls": accumulated_delta.tool_calls,
-        #             }
-        #             llm_event.prompt_tokens = chunk.data.usage.prompt_tokens
-        #             llm_event.completion_tokens = (
-        #                 chunk.data.usage.completion_tokens
-        #             )
-        #             llm_event.end_timestamp = get_ISO_time()
-        #             self._safe_record(session, llm_event)
+            except Exception as e:
+                self._safe_record(
+                    session, ErrorEvent(trigger_event=llm_event, exception=e)
+                )
 
-        #     except Exception as e:
-        #         self._safe_record(
-        #             session, ErrorEvent(trigger_event=llm_event, exception=e)
-        #         )
+                kwargs_str = pprint.pformat(kwargs)
+                chunk = pprint.pformat(chunk)
+                logger.warning(
+                    f"Unable to parse a chunk for LLM call. Skipping upload to AgentOps\n"
+                    f"chunk:\n {chunk}\n"
+                    f"kwargs:\n {kwargs_str}\n"
+                )
 
-        #         kwargs_str = pprint.pformat(kwargs)
-        #         chunk = pprint.pformat(chunk)
-        #         logger.warning(
-        #             f"Unable to parse a chunk for LLM call. Skipping upload to AgentOps\n"
-        #             f"chunk:\n {chunk}\n"
-        #             f"kwargs:\n {kwargs_str}\n"
-        #         )
+        # if the response is a generator, decorate the generator
+        # if isinstance(response, ChatCompletionChunk):
+
+        #     def generator():
+        #         for chunk in response:
+        #             handle_stream_chunk(chunk)
+        #             yield chunk
+
+        #     return generator()
+
+        # For synchronous Stream
+        if isinstance(response, Stream):
+
+            def generator():
+                for chunk in response:
+                    handle_stream_chunk(chunk)
+                    yield chunk
+
+            return generator()
+
+        # For asynchronous AsyncStream
+        if isinstance(response, AsyncStream):
+
+            async def async_generator():
+                async for chunk in response:
+                    handle_stream_chunk(chunk)
+                    yield chunk
+
+            return async_generator()
+        
 
         # Handle object responses
         try:
@@ -116,15 +142,15 @@ class AI21Provider(InstrumentedProvider):
 
     def override(self):
         self._override_completion()
-        self._override_complete_async()
-        self._override_stream()
-        self._override_stream_async()
+        self._override_completion_async()
+        # self._override_stream()
+        # self._override_stream_async()
 
     def _override_completion(self):
         from ai21.clients.studio.resources.chat import ChatCompletions
 
-        global original_chat
-        original_chat = ChatCompletions.create
+        global original_create
+        original_create = ChatCompletions.create
 
         def patched_function(*args, **kwargs):
             # Call the original function with its original arguments
@@ -132,26 +158,73 @@ class AI21Provider(InstrumentedProvider):
             session = kwargs.get("session", None)
             if "session" in kwargs.keys():
                 del kwargs["session"]
-            result = original_chat(*args, **kwargs)
+            result = original_create(*args, **kwargs)
             return self.handle_response(result, kwargs, init_timestamp, session=session)
 
         # Override the original method with the patched one
         ChatCompletions.create = patched_function
 
-    def _override_complete_async(self):
-        pass
+    def _override_completion_async(self):
+        from ai21.clients.studio.resources.chat import AsyncChatCompletions
 
-    def _override_stream(self):
-        pass
+        global original_create_async
+        original_create_async = AsyncChatCompletions.create
 
-    def _override_stream_async(self):
-        pass
+        async def patched_function(*args, **kwargs):
+            # Call the original function with its original arguments
+            init_timestamp = get_ISO_time()
+            session = kwargs.get("session", None)
+            if "session" in kwargs.keys():
+                del kwargs["session"]
+            result = await original_create_async(*args, **kwargs)
+            return self.handle_response(result, kwargs, init_timestamp, session=session)
+
+        # Override the original method with the patched one
+        AsyncChatCompletions.create = patched_function
+
+    # def _override_stream(self):
+    #     from ai21.clients.studio.resources.chat import ChatCompletions
+
+    #     global original_stream
+    #     original_stream = ChatCompletions.create
+
+    #     def patched_function(*args, **kwargs):
+    #         # Call the original function with its original arguments
+    #         init_timestamp = get_ISO_time()
+    #         session = kwargs.get("session", None)
+    #         if "session" in kwargs.keys():
+    #             del kwargs["session"]
+    #         result = original_stream(*args, **kwargs)
+    #         return self.handle_response(result, kwargs, init_timestamp, session=session)
+
+    #     ChatCompletions.create = patched_function
+
+    # def _override_stream_async(self):
+    #     from ai21.clients.studio.resources.chat import AsyncChatCompletions
+
+    #     global original_stream_async
+    #     original_stream_async = AsyncChatCompletions.create
+
+    #     async def patched_function(*args, **kwargs):
+    #         # Call the original function with its original arguments
+    #         init_timestamp = get_ISO_time()
+    #         session = kwargs.get("session", None)
+    #         if "session" in kwargs.keys():
+    #             del kwargs["session"]
+    #         result = await original_stream_async(*args, **kwargs)
+    #         return self.handle_response(result, kwargs, init_timestamp, session=session)
+
+    #     AsyncChatCompletions.create = patched_function
 
     def undo_override(self):
         if (
             self.original_create is not None
             and self.original_create_async is not None
-            and self.original_stream is not None
-            and self.original_stream_async is not None
+            # and self.original_stream is not None
+            # and self.original_stream_async is not None
         ):
-            pass
+            from ai21.clients.studio.resources.chat import ChatCompletions, AsyncChatCompletions
+
+            ChatCompletions.create = self.original_create
+            AsyncChatCompletions.create = self.original_create_async
+
