1b8f473f20d0071ec19f79492137eb7275213cea
Pratyush Shukla <ps4534@nyu.edu>
2024-11-05 12:52:24 +0530

    [FEATURE] Add support for AI21 via the `ai21` Python SDK (#382)
    * add initial files for support
    
    * working sync client
    
    * stream not working
    
    * updated examples notebook for current testing
    
    * fix for `delta.content` and cleanup
    
    * cleanup again
    
    * cleanup and add tool event
    
    * structure examples notebook
    
    * add contextual answers tracking
    
    * cleanup example notebook
    
    * create testing file
    
    * clean example notebook again
    
    * rename examples directory
    
    * updated docs page
    
    * wrap `chunk.choices[0].delta.content` in `str(...)`
    
    * update doc
    
    ---------
    
    Co-authored-by: reibs <areibman@gmail.com>

--- agentops/llms/__init__.py
+++ agentops/llms/__init__.py
@@ -13,6 +13,7 @@ from .litellm import LiteLLMProvider
 from .ollama import OllamaProvider
 from .openai import OpenAiProvider
 from .anthropic import AnthropicProvider
+from .ai21 import AI21Provider
 
 original_func = {}
 original_create = None
@@ -38,6 +39,12 @@ class LlmTracker:
         },
         "anthropic": {
             "0.32.0": ("completions.create",),
+        },
+        "ai21": {
+            "2.0.0": (
+                "chat.completions.create",
+                "client.answer.create",
+            ),
         },
     }
 
@@ -135,6 +142,22 @@ class LlmTracker:
                             f"Only Anthropic>=0.32.0 supported. v{module_version} found."
                         )
 
+                if api == "ai21":
+                    module_version = version(api)
+
+                    if module_version is None:
+                        logger.warning(
+                            f"Cannot determine AI21 version. Only AI21>=2.0.0 supported."
+                        )
+
+                    if Version(module_version) >= parse("2.0.0"):
+                        provider = AI21Provider(self.client)
+                        provider.override()
+                    else:
+                        logger.warning(
+                            f"Only AI21>=2.0.0 supported. v{module_version} found."
+                        )
+
     def stop_instrumenting(self):
         OpenAiProvider(self.client).undo_override()
         GroqProvider(self.client).undo_override()
@@ -142,3 +165,4 @@ class LlmTracker:
         LiteLLMProvider(self.client).undo_override()
         OllamaProvider(self.client).undo_override()
         AnthropicProvider(self.client).undo_override()
+        AI21Provider(self.client).undo_override()
