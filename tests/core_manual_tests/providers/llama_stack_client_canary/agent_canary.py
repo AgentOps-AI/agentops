27db3df82286cb26a7864014b0ebff01de6ed925
tad dy <a@a.com>
2024-12-07 23:49:56 -0500

    saving working agent_canary test - works in Python notebook AND in script

--- tests/core_manual_tests/providers/llama_stack_client_canary/agent_canary.py
+++ tests/core_manual_tests/providers/llama_stack_client_canary/agent_canary.py
@@ -1,27 +1,28 @@
 import asyncio
 import os
+from dotenv import load_dotenv
 
 from llama_stack_client import LlamaStackClient
 from llama_stack_client.lib.agents.agent import Agent
 from llama_stack_client.lib.agents.event_logger import EventLogger
-from llama_stack_client.types import Attachment
 from llama_stack_client.types.agent_create_params import AgentConfig
 
-import agentops
-
-LLAMA_STACK_PORT = 5001
-INFERENCE_MODEL = "meta-llama/Llama-3.2-1B-Instruct"
+load_dotenv()
 
 # import debugpy
 # debugpy.listen(5678)
 # debugpy.wait_for_client()
 
-agentops.init(default_tags=["llama-stack-client-example"], auto_start_session=False)
+import agentops # type: ignore
+agentops.init(os.getenv("AGENTOPS_API_KEY"), default_tags=["llama-stack-client-example"], auto_start_session=False)
 
+LLAMA_STACK_HOST = "0.0.0.0"
+LLAMA_STACK_PORT = 5001
+INFERENCE_MODEL = "meta-llama/Llama-3.2-1B-Instruct"
 
 async def agent_test():
     client = LlamaStackClient(
-        base_url=f"http://localhost:{LLAMA_STACK_PORT}",
+        base_url=f"http://{LLAMA_STACK_HOST}:{LLAMA_STACK_PORT}",
     )
 
     available_shields = [shield.identifier for shield in client.shields.list()]
@@ -76,12 +77,9 @@ async def agent_test():
             session_id=session_id,
         )
 
-        print("Response: ", response)
-
-        # for log in EventLogger().log(response):
-        #     log.print()
-
+        for log in EventLogger().log(response):
+            log.print()
 
 agentops.start_session()
 asyncio.run(agent_test())
-agentops.end_session("Success")
+agentops.end_session(end_state="Success")
