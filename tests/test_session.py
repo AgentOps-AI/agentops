4fd051afb139fe779cd1c8a3c571dedadd2394ea
Pratyush Shukla <ps4534@nyu.edu>
2025-01-07 21:06:15 +0530

    use `_span_processor` instead of `_otel_exporter`

--- tests/test_session.py
+++ tests/test_session.py
@@ -392,7 +392,7 @@ class TestSessionExporter:
         agentops.init(api_key=self.api_key, max_wait_time=50, auto_start_session=False)
         self.session = agentops.start_session()
         assert self.session is not None  # Verify session was created
-        self.exporter = self.session._otel_exporter
+        self.exporter = self.session._span_processor
 
     def teardown_method(self):
         """Clean up after each test"""
