e7a80350219f5923a8ec6029d88457a52b4e2612
Pratyush Shukla <ps4534@nyu.edu>
2025-01-09 22:05:35 +0530

    Revert "remove redundant code in ai21"
    This reverts commit 1a0740f2a4111f1a1b0fd5545483528c04158fb9.

--- agentops/llms/providers/ai21.py
+++ agentops/llms/providers/ai21.py
@@ -29,6 +29,7 @@ class AI21Provider(InstrumentedProvider):
         from ai21.stream.async_stream import AsyncStream
         from ai21.models.chat.chat_completion_chunk import ChatCompletionChunk
         from ai21.models.chat.chat_completion_response import ChatCompletionResponse
+        from ai21.models.responses.answer_response import AnswerResponse
 
         llm_event = LLMEvent(init_timestamp=init_timestamp, params=kwargs)
         action_event = ActionEvent(init_timestamp=init_timestamp, params=kwargs)
@@ -107,15 +108,27 @@ class AI21Provider(InstrumentedProvider):
 
         # Handle object responses
         try:
-            llm_event.returns = response
-            llm_event.agent_id = check_call_stack_for_agent_id()
-            llm_event.model = kwargs["model"]
-            llm_event.prompt = [message.model_dump() for message in kwargs["messages"]]
-            llm_event.prompt_tokens = response.usage.prompt_tokens
-            llm_event.completion = response.choices[0].message.model_dump()
-            llm_event.completion_tokens = response.usage.completion_tokens
-            llm_event.end_timestamp = get_ISO_time()
-            self._safe_record(session, llm_event)
+            if isinstance(response, ChatCompletionResponse):
+                llm_event.returns = response
+                llm_event.agent_id = check_call_stack_for_agent_id()
+                llm_event.model = kwargs["model"]
+                llm_event.prompt = [message.model_dump() for message in kwargs["messages"]]
+                llm_event.prompt_tokens = response.usage.prompt_tokens
+                llm_event.completion = response.choices[0].message.model_dump()
+                llm_event.completion_tokens = response.usage.completion_tokens
+                llm_event.end_timestamp = get_ISO_time()
+                self._safe_record(session, llm_event)
+
+            elif isinstance(response, AnswerResponse):
+                action_event.returns = response
+                action_event.agent_id = check_call_stack_for_agent_id()
+                action_event.action_type = "Contextual Answers"
+                action_event.logs = [
+                    {"context": kwargs["context"], "question": kwargs["question"]},
+                    response.model_dump() if response.model_dump() else None,
+                ]
+                action_event.end_timestamp = get_ISO_time()
+                self._safe_record(session, action_event)
 
         except Exception as e:
             self._safe_record(session, ErrorEvent(trigger_event=llm_event, exception=e))
@@ -132,6 +145,8 @@ class AI21Provider(InstrumentedProvider):
     def override(self):
         self._override_completion()
         self._override_completion_async()
+        self._override_answer()
+        self._override_answer_async()
 
     def _override_completion(self):
         from ai21.clients.studio.resources.chat import ChatCompletions
@@ -169,6 +184,42 @@ class AI21Provider(InstrumentedProvider):
         # Override the original method with the patched one
         AsyncChatCompletions.create = patched_function
 
+    def _override_answer(self):
+        from ai21.clients.studio.resources.studio_answer import StudioAnswer
+
+        global original_answer
+        original_answer = StudioAnswer.create
+
+        def patched_function(*args, **kwargs):
+            # Call the original function with its original arguments
+            init_timestamp = get_ISO_time()
+
+            session = kwargs.get("session", None)
+            if "session" in kwargs.keys():
+                del kwargs["session"]
+            result = original_answer(*args, **kwargs)
+            return self.handle_response(result, kwargs, init_timestamp, session=session)
+
+        StudioAnswer.create = patched_function
+
+    def _override_answer_async(self):
+        from ai21.clients.studio.resources.studio_answer import AsyncStudioAnswer
+
+        global original_answer_async
+        original_answer_async = AsyncStudioAnswer.create
+
+        async def patched_function(*args, **kwargs):
+            # Call the original function with its original arguments
+            init_timestamp = get_ISO_time()
+
+            session = kwargs.get("session", None)
+            if "session" in kwargs.keys():
+                del kwargs["session"]
+            result = await original_answer_async(*args, **kwargs)
+            return self.handle_response(result, kwargs, init_timestamp, session=session)
+
+        AsyncStudioAnswer.create = patched_function
+
     def undo_override(self):
         if (
             self.original_create is not None
@@ -180,6 +231,12 @@ class AI21Provider(InstrumentedProvider):
                 ChatCompletions,
                 AsyncChatCompletions,
             )
+            from ai21.clients.studio.resources.studio_answer import (
+                StudioAnswer,
+                AsyncStudioAnswer,
+            )
 
             ChatCompletions.create = self.original_create
             AsyncChatCompletions.create = self.original_create_async
+            StudioAnswer.create = self.original_answer
+            AsyncStudioAnswer.create = self.original_answer_async
