da571b0ba0fff5e78faf3bc0d53165e9f18868a2
Pratyush Shukla <ps4534@nyu.edu>
2025-01-23 12:23:23 +0530

    updated test code

--- tests/langchain_handlers/_test_langchain_handler.py
+++ tests/langchain_handlers/_test_langchain_handler.py
@@ -1,9 +1,9 @@
 import asyncio
 import os
-from langchain.chat_models import ChatOpenAI
-from langchain.agents import initialize_agent, AgentType
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.agents import tool, AgentExecutor, create_openai_tools_agent
 from dotenv import load_dotenv
-from langchain.agents import tool
 from agentops.partners.langchain_callback_handler import (
     LangchainCallbackHandler as AgentOpsLangchainCallbackHandler,
     AsyncLangchainCallbackHandler as AgentOpsAsyncLangchainCallbackHandler,
@@ -14,9 +14,9 @@ load_dotenv()
 AGENTOPS_API_KEY = os.environ.get("AGENTOPS_API_KEY")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-agentops_handler = AgentOpsLangchainCallbackHandler(api_key=AGENTOPS_API_KEY, tags=["Langchain Example"])
+agentops_handler = AgentOpsLangchainCallbackHandler(api_key=AGENTOPS_API_KEY, default_tags=["Langchain", "Sync Handler Test"])
 
-llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, callbacks=[agentops_handler], model="gpt-3.5-turbo")
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, callbacks=[agentops_handler], model="gpt-4o-mini")
 
 
 @tool
@@ -34,38 +34,35 @@ tools = [find_movie]
 for t in tools:
     t.callbacks = [agentops_handler]
 
-agent = initialize_agent(
-    tools,
-    llm,
-    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True,
-    callbacks=[agentops_handler],  # You must pass in a callback handler to record your agent
-    handle_parsing_errors=True,
-)
+########
+# Sync
+########
 
+prompt = ChatPromptTemplate([
+    ("system", "You are a helpful assistant. Respond only in Spanish."),
+    ("user", "{input}"),
+    ("system", "Here is the current conversation state:\n{agent_scratchpad}"),
+])
 
-agent.run("What comedies are playing?", callbacks=[agentops_handler])
+agent = create_openai_tools_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools)
+
+agent_executor.invoke({"input": "What comedies are playing?"}, config={"callback": [agentops_handler]})
 
 
 ########
 # Async
+########
 
-agentops_handler = AgentOpsAsyncLangchainCallbackHandler(api_key=AGENTOPS_API_KEY, tags=["Async Example"])
+agentops_handler = AgentOpsAsyncLangchainCallbackHandler(api_key=AGENTOPS_API_KEY, default_tags=["Langchain", "Async Handler Test"])
 
-llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, callbacks=[agentops_handler], model="gpt-3.5-turbo")
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, callbacks=[agentops_handler], model="gpt-4o-mini", streaming=True)
 
-agent = initialize_agent(
-    tools,
-    llm,
-    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True,
-    callbacks=[agentops_handler],  # You must pass in a callback handler to record your agent
-    handle_parsing_errors=True,
-)
-
+agent = create_openai_tools_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools)
 
 async def run_async():
-    await agent.run("What comedies are playing?", callbacks=[agentops_handler])
+    await agent_executor.ainvoke({"input": "What comedies are playing?"}, config={"callback": [agentops_handler]})
 
 
 asyncio.run(run_async())
