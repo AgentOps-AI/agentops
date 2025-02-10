f15ffb0714c3b40afb9b06fb704a73e5a782b144
Teo <teocns@gmail.com>
2025-02-11 01:28:02 +0200

    InstrumentedBase: init_timestamp handling
    Signed-off-by: Teo <teocns@gmail.com>

--- agentops/telemetry/model.py
+++ agentops/telemetry/model.py
@@ -2,23 +2,35 @@
 
 from dataclasses import dataclass, field
 from typing import Optional
+
+from opentelemetry import trace
 from opentelemetry.trace import Span
 
+from agentops.helpers import get_ISO_time, iso_to_unix_nano
 from agentops.log_config import logger
 
 
 @dataclass
 class InstrumentedBase:
     """Base class for objects that have timing and OpenTelemetry instrumentation.
-    
+
     Provides consistent handling of:
     - init_timestamp and end_timestamp fields
     - span association and lifecycle
     - timing-related properties and methods
     """
+
+    init_timestamp: Optional[str] = field(default=None)  # Allow setting in __init__
+    end_timestamp: Optional[str] = field(default=None)  # Allow setting in __init__
     _span: Optional[Span] = field(default=None, init=False)
-    init_timestamp: Optional[str] = field(default=None, init=False)
-    end_timestamp: Optional[str] = field(default=None, init=False)
+
+    def __post_init__(self):
+        """Create span and set timestamps if provided"""
+        tracer = trace.get_tracer(__name__)
+        span = tracer.start_as_current_span(  # Use start_as_current_span to let OTEL set the current span context
+            self.__class__.__name__, start_time=iso_to_unix_nano(self.init_timestamp or get_ISO_time())
+        )
+        self._span = span
 
     @property
     def span(self) -> Optional[Span]:
@@ -39,4 +51,4 @@ class InstrumentedBase:
         """End the associated span if it exists"""
         if self._span is not None:
             logger.debug("Ending span")
-            self._span.end() 
+            self._span.end()
