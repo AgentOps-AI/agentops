206dba04ff3736f35c3001a24395fa29f912cc90
Teo <teocns@gmail.com>
2025-02-26 01:11:15 +0200

    +exceptions.py
    Signed-off-by: Teo <teocns@gmail.com>

--- agentops/exceptions.py
+++ agentops/exceptions.py
@@ -7,10 +7,19 @@ class MultiSessionException(Exception):
 
 
 class NoSessionException(Exception):
-    def __init__(self, message):
+    def __init__(self, message = "No session found"):
         super().__init__(message)
 
+class NoApiKeyException(Exception):
+    def __init__(self, message = "Could not initialize AgentOps client - API Key is missing."
+                + "\n\t    Find your API key at https://app.agentops.ai/settings/projects"):
+        super().__init__(message)
 
 class ApiServerException(Exception):
     def __init__(self, message):
+        super().__init__(message)
+        
+
+class AgentOpsClientNotInitializedException(RuntimeError):
+    def __init__(self, message = "AgentOps client must be initialized before using this feature"):
         super().__init__(message)
