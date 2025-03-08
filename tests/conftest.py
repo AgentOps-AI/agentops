8e0a95076ec5a17edbd1e2b23ee465c13c2ca884
Travis Dent <tcdent@gmail.com>
2025-03-07 20:31:25 -0800

    Suppress otel shutdown log messages in testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,17 @@
+import logging
 import pytest
+from unittest import mock
+
+@pytest.fixture(scope="session", autouse=True)
+def suppress_otel_shutdown_warnings():
+    """opentelemetry prints a ton of warnings on shutown, this fixture suppresses them"""
+    # messages like:
+    # Already shutdown, ignoring call to force_flush().
+    # Already shutdown, dropping span.
+    # I would prefer to filter those messages specifically, but adding a filter
+    # to the logger doesn't seem to work.
+    logger = logging.getLogger("opentelemetry.sdk.trace.export")
+    logger.disabled = True
 
 
 @pytest.fixture
