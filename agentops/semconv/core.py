d51e9d2c023ea78124513befd4b30ab78ffa3ce6
Teo <teocns@gmail.com>
2025-03-11 20:02:48 +0200

    semconv
    Signed-off-by: Teo <teocns@gmail.com>

--- agentops/semconv/core.py
+++ agentops/semconv/core.py
@@ -1,8 +1,12 @@
 """Core attributes applicable to all spans."""
 
+
 class CoreAttributes:
     """Core attributes applicable to all spans."""
-    
+
     # Status attributes
     ERROR_TYPE = "error.type"          # Type of error if status is error
     ERROR_MESSAGE = "error.message"    # Error message if status is error
+
+    IN_FLIGHT = "agentops.in-flight"   # Whether the span is in-flight
+    EXPORT_IMMEDIATELY = "agentops.export.immediate"  # Whether the span should be exported immediately
