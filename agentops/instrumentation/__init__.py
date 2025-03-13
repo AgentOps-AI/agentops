7a4d27d87c2be94e58efa74d44d7ed9597db8588
Pratyush Shukla <ps4534@nyu.edu>
2025-03-13 11:26:34 +0530

    remove incomplete instrumentations from the sdk

--- agentops/instrumentation/__init__.py
+++ agentops/instrumentation/__init__.py
@@ -16,21 +16,22 @@ _active_instrumentors: list[BaseInstrumentor] = []
 @dataclass
 class InstrumentorLoader:
     """
-    Represents a dynamically-loadable instrumentor.
-    
-    This class is used to load and activate instrumentors based on their module
-and class names.
-    We use the `provider_import_name` to determine if the library is installed i
-n the environment.
-    
-    `modue_name` is the name of the module to import from.
-    `class_name` is the name of the class to instantiate from the module.
-    `provider_import_name` is the name of the package to check for availability.
+        Represents a dynamically-loadable instrumentor.
+
+        This class is used to load and activate instrumentors based on their module
+    and class names.
+        We use the `provider_import_name` to determine if the library is installed i
+    n the environment.
+
+        `modue_name` is the name of the module to import from.
+        `class_name` is the name of the class to instantiate from the module.
+        `provider_import_name` is the name of the package to check for availability.
     """
+
     module_name: str
     class_name: str
     provider_import_name: str
-    
+
     @property
     def module(self) -> ModuleType:
         """Reference to the instrumentor module."""
@@ -52,39 +53,24 @@ n the environment.
 
 available_instrumentors: list[InstrumentorLoader] = [
     InstrumentorLoader(
-        module_name='opentelemetry.instrumentation.openai', 
-        class_name='OpenAIInstrumentor', 
-        provider_import_name='openai', 
-    ), 
-    InstrumentorLoader(
-        module_name='opentelemetry.instrumentation.anthropic', 
-        class_name='AnthropicInstrumentor', 
-        provider_import_name='anthropic', 
+        module_name="opentelemetry.instrumentation.openai",
+        class_name="OpenAIInstrumentor",
+        provider_import_name="openai",
     ),
     InstrumentorLoader(
-        module_name='opentelemetry.instrumentation.cohere', 
-        class_name='CohereInstrumentor', 
-        provider_import_name='cohere', 
+        module_name="opentelemetry.instrumentation.anthropic",
+        class_name="AnthropicInstrumentor",
+        provider_import_name="anthropic",
     ),
     InstrumentorLoader(
-        module_name='opentelemetry.instrumentation.crewai', 
-        class_name='CrewAIInstrumentor', 
-        provider_import_name='crewai', 
+        module_name="opentelemetry.instrumentation.crewai",
+        class_name="CrewAIInstrumentor",
+        provider_import_name="crewai",
     ),
     InstrumentorLoader(
-        module_name='opentelemetry.instrumentation.groq', 
-        class_name='GroqInstrumentor', 
-        provider_import_name='groq', 
-    ),
-    InstrumentorLoader(
-        module_name='opentelemetry.instrumentation.mistralai', 
-        class_name='MistralAiInstrumentor', 
-        provider_import_name='mistralai', 
-    ),
-    InstrumentorLoader(
-        module_name='opentelemetry.instrumentation.agents', 
-        class_name='AgentsInstrumentor', 
-        provider_import_name='agents', 
+        module_name="opentelemetry.instrumentation.agents",
+        class_name="AgentsInstrumentor",
+        provider_import_name="agents",
     ),
 ]
 
@@ -93,13 +79,15 @@ def instrument_one(loader: InstrumentorLoader) -> Optional[BaseInstrumentor]:
     """Instrument a single instrumentor."""
     if not loader.should_activate:
         # this package is not in the environment; skip
-        logger.debug(f"Package {loader.provider_import_name} not found; skipping instrumentation of {loader.class_name}")
+        logger.debug(
+            f"Package {loader.provider_import_name} not found; skipping instrumentation of {loader.class_name}"
+        )
         return None
-    
+
     instrumentor = loader.get_instance()
     instrumentor.instrument(tracer_provider=TracingCore.get_instance()._provider)
     logger.debug(f"Instrumented {loader.class_name}")
-    
+
     return instrumentor
 
 
@@ -109,17 +97,17 @@ def instrument_all():
     This function is called when `instrument_llm_calls` is enabled.
     """
     global _active_instrumentors
-    
+
     if len(_active_instrumentors):
         logger.debug("Instrumentors have already been populated.")
         return
-    
+
     for loader in available_instrumentors:
         if loader.class_name in _active_instrumentors:
             # already instrumented
             logger.debug(f"Instrumentor {loader.class_name} has already been instrumented.")
             return None
-        
+
         instrumentor = instrument_one(loader)
         if instrumentor is not None:
             _active_instrumentors.append(instrumentor)
