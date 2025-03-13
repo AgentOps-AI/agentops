a095fac8e30a79fd4c5d7902aff22ccfb3198db5
Teo <teocns@gmail.com>
2025-03-14 00:22:34 +0200

    examples/sdk/basic: add a second nest level
    Signed-off-by: Teo <teocns@gmail.com>

--- examples/sdk/basic.py
+++ examples/sdk/basic.py
@@ -1,15 +1,20 @@
-from agentops.sdk.decorators.agentops import session, agent, operation, record
 import agentops
-
+from agentops.sdk.decorators.agentops import agent, operation, record, session
 
 agentops.init()
 
 
 @agent
 class Agent:
+
+    @operation
+    def nested_operation(self):
+        print("Hello, world!")
+
     @operation
     def my_operation(self):
         print("Hello, world!")
+        self.nested_operation()
 
 
 @session
