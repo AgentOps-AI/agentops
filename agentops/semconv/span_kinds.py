544dbbf583d7b8d2d3153da4fc8c707365d7e2a7
michi-okahata <michi.okahata@outlook.com>
2025-07-14 11:54:45 -0700

    semconv changes

--- agentops/semconv/span_kinds.py
+++ agentops/semconv/span_kinds.py
@@ -13,7 +13,6 @@ class AgentOpsSpanKindValues(Enum):
     AGENT = "agent"
     TOOL = "tool"
     LLM = "llm"
-    TEAM = "team"
     CHAIN = "chain"
     TEXT = "text"
     GUARDRAIL = "guardrail"
@@ -41,7 +40,6 @@ class SpanKind:
     AGENT = AgentOpsSpanKindValues.AGENT.value
     TOOL = AgentOpsSpanKindValues.TOOL.value
     LLM = AgentOpsSpanKindValues.LLM.value
-    TEAM = AgentOpsSpanKindValues.TEAM.value
     UNKNOWN = AgentOpsSpanKindValues.UNKNOWN.value
     CHAIN = AgentOpsSpanKindValues.CHAIN.value
     TEXT = AgentOpsSpanKindValues.TEXT.value
