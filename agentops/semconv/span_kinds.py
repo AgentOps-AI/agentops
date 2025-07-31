21c2b12f725efd1930cf212d1f087c7326040d8c
koreandinner <154290493+michi-okahata@users.noreply.github.com>
2025-07-31 10:22:48 -0700

    DSPy Callback Handler (#1161)
    * fix mcp docs
    
    * semconv changes
    
    * first pass
    
    * lol
    
    * second pass
    
    * dspy first draft
    
    * dspy second draft, removed misc.
    
    * decent, minimally compliant with semconv
    
    * precommit fix
    
    * dspy integration example
    
    ---------
    
    Co-authored-by: Pratyush Shukla <ps4534@nyu.edu>

--- agentops/semconv/span_kinds.py
+++ agentops/semconv/span_kinds.py
@@ -13,7 +13,6 @@ class AgentOpsSpanKindValues(Enum):
     AGENT = "agent"
     TOOL = "tool"
     LLM = "llm"
-    TEAM = "team"
     CHAIN = "chain"
     TEXT = "text"
     GUARDRAIL = "guardrail"
@@ -42,7 +41,6 @@ class SpanKind:
     AGENT = AgentOpsSpanKindValues.AGENT.value
     TOOL = AgentOpsSpanKindValues.TOOL.value
     LLM = AgentOpsSpanKindValues.LLM.value
-    TEAM = AgentOpsSpanKindValues.TEAM.value
     UNKNOWN = AgentOpsSpanKindValues.UNKNOWN.value
     CHAIN = AgentOpsSpanKindValues.CHAIN.value
     TEXT = AgentOpsSpanKindValues.TEXT.value
